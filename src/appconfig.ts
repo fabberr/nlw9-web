e124cb31137ac95fb67bc3177f4abb7709f80c0f
Fabricio Milanez <fabriciomilanez@protonmail.com>
2022-09-17 18:06:16 -0300

    [misc] refactored documentation

--- src/appconfig.ts
+++ src/appconfig.ts
@@ -2,11 +2,13 @@
  * File: appconfig.ts
  * 
  * This file ensures that certain environment variables loaded into Vite will 
- * have default values if they're not present in a dotenv file and exports them 
- * alongside additional variables derived from their values.
+ * have default values if they are not present in a dotenv fil, then exports 
+ * them alongside additional variables derived from their values.
 */
 
-export const API_HOSTNAME = import.meta.env.VITE_API_HOSTNAME || 'localhost';
-export const API_PORT     = import.meta.env.VITE_API_PORT     || '3333';
+// from ENV
+export const API_HOSTNAME = import.meta.env.VITE_API_HOSTNAME || 'localhost'; /** LAN IP adress of the server or `localhost`. */
+export const API_PORT     = import.meta.env.VITE_API_PORT     || '3333';      /** Port the server is listening for connection on. */
 
+// additional values
 export const API_BASE_URL = `${API_HOSTNAME}:${API_PORT}`;
